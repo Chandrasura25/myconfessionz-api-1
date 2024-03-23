b864e66071682cbe3d6c298a9183547744e0bc92
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-03-23 05:15:50 +0100

    auth

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -11,7 +11,7 @@ class SessionController extends Controller
 {
     public function processSession(Request $request, $id)
     {
-        $user = Auth::user();
+        $user = auth()->user();
         $counselorId = $id;
         
         // Check if there is an existing session with the same counselor and status is true
@@ -52,25 +52,29 @@ class SessionController extends Controller
         }
     }
 
-    public function checkSession($id)
+    public function checkSession($counselorId)
     {
-        $user = Auth::user();
-        $counselorId = $id;
-        
-        $activeSession = Session::where('user_id', $user->id)
-                                ->where('counselor_id', $counselorId)
-                                ->where('status', true)
-                                ->first();
+        if (auth()->check()) {
+            $user = auth()->user();
 
-        if ($activeSession) {
-            return response()->json(['session' => $activeSession], 200);
+            $activeSession = Session::where('user_id', $user->id)
+                                    ->where('counselor_id', $counselorId)
+                                    ->where('status', true)
+                                    ->first();
+
+            if ($activeSession) {
+                return response()->json(['session' => $activeSession], 200);
+            } else {
+                return response()->json(['error' => 'No active session found with this counselor'], 404);
+            }
         } else {
-            return response()->json(['error' => 'No active session found with this counselor'], 404);
+            return response()->json(['error' => 'Unauthenticated'], 401);
         }
     }
+
     public function endSession(Request $request, $counselorId)
     {
-        $user = Auth::user();
+        $user = auth()->user();
         
         $finishedSession = Session::where('user_id', $user->id)
                                   ->where('counselor_id', $counselorId)
