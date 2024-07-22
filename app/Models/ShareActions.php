e131567e0819732d7cabd75eb7605679b2c71e76
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-07-22 17:01:52 +0100

    firebase

--- app/Models/ShareActions.php
+++ app/Models/ShareActions.php
@@ -4,7 +4,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ShareAction extends Model
+class ShareActions extends Model
 {
     use HasFactory;
 
